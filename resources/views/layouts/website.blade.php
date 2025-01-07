fbcfec05c430043b15bd12ff9903011b3ea5a34d
Fred Nyachomo <nyachomofred@gmail.com>
2025-01-07 08:17:46 +0300

    second commit

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -81,7 +81,7 @@
       <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
         <div class="container">
           <a class="navbar-brand" href="index.html" >
-            <img src="{{asset('website/logo/logo.jpeg')}}" height="100px">
+            <img src="{{asset('website/logo/logo.jpeg')}}" height="70px">
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
